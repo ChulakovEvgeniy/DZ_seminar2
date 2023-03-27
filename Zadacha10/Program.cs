a763af9cece267d10c72054ea075b648266db5e0
«ChulakovEvgeniy» <>
2023-03-27 23:20:30 +0300

    решение 10 задачи

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите любое число от 100 до 999 включительно");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num / 10 % 10;
+System.Console.WriteLine(result);
