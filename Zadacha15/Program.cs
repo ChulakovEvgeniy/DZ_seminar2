190143485644caade1a78374132a5686fec8242a
«ChulakovEvgeniy» <>
2023-04-02 20:31:19 +0300

    решение 15 задачи

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write("Введите число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 5) System.Console.WriteLine("не выходной");
+else if (num > 5 && num <= 7) System.Console.WriteLine("выходной");
+else System.Console.WriteLine("такого дня не существует");
